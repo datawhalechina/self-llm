67be0c902e49597887538897f866826979925ec2
KMnO4-zx <1021385881@qq.com>
2024-02-05 22:54:27 +0800

    Update training configuration in train.sh

--- MiniCPM/train.sh
+++ MiniCPM/train.sh
@@ -2,9 +2,9 @@ num_gpus=1
 
 deepspeed --num_gpus $num_gpus train.py \
     --deepspeed ./ds_config.json \
-    --output_dir="./output/Qwen" \
-    --per_device_train_batch_size=1 \
-    --gradient_accumulation_steps=1 \
+    --output_dir="./output/MiniCPM" \
+    --per_device_train_batch_size=4 \
+    --gradient_accumulation_steps=4 \
     --logging_steps=10 \
     --num_train_epochs=3 \
     --save_steps=500 \
